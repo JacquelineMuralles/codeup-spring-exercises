deb5899417029828fef472d4b7273d0cb5f7e37d
Jacqueline Muralles <jacquelinenmuralles@gmail.com>
2020-05-11 13:36:34 -0500

    first exercises complete for spring

---
+++ src/main/java/com/example/springblogapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springblogapp;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//@Controller//dictates that this class is a controller in (MVC)
+public class HelloController {
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello from Spring!";
+//    }
+
+}
---
+++ src/main/java/com/example/springblogapp/MathController.java
@@ -0,0 +1,38 @@
+package com.example.springblogapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//@Controller
+public class MathController {
+    //method that listens for
+    //    /{operation}/{firstnum}/{keyword}/{secondnum}
+
+//        @GetMapping("/{opperation}/{a}/{keyword}/{b}")
+//        @ResponseBody
+//    public String addition(@PathVariable String operation, @PathVariable int a, @PathVariable String keyword, @PathVariable int b){
+//            // decide what to do based on "add" "subtract"...etc from 'operation' PathVariable
+//            switch (operation){
+//                case "add":
+//                    int sum;
+//                    sum = a + b;;
+//                    return Integer.toString(sum);
+//                case "subtract":
+//                    int diff;
+//                    diff = b - a;
+//                    return Integer.toString(diff);
+//                case "multiply":
+//                    int product;
+//                    product = a * b;
+//                    return Integer.toString(product);
+//                case "divide":
+//                    int quotient;
+//                    quotient = a / b;
+//                    if (a % b != 0){
+//                        quotient =
+//                    }
+//            }
+//    }
+}
