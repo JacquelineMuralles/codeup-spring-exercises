33951be3357ff4d039cddbefb1589b07105ba6f7
Jacqueline Muralles <jacquelinenmuralles@gmail.com>
2020-05-12 17:24:27 -0500

    completed dice game and added fragments to my partials folder

---
+++ src/main/java/com/example/springblogapp/controller/AdController.java
@@ -0,0 +1,19 @@
+package com.example.springblogapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@Controller
+public class AdController {
+
+//    @GetMapping("/ads")
+//    public String showAllAds(Model model){
+//        List<Ad> adList = new ArrayList<>();
+//        Ad ad = new Ad("some car", "color of car");
+//        adList.add(ad);
+//    }
+}
--- src/main/java/com/example/springblogapp/controller/HelloController.java
+++ src/main/java/com/example/springblogapp/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.springblogapp;
+package com.example.springblogapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
